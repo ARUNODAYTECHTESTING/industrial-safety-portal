56109cdcb973ba4544250a31ce2b627dd11ab945
Mohd Saquib <saqib@dataviv.in>
2025-02-12 23:37:03 +0530

    update usr.jso

--- account/models.py
+++ account/models.py
@@ -65,10 +65,10 @@ class User(AbstractBaseUser, PermissionsMixin):
 
         ]
 
-    # def save(self, *args, **kwargs):
-    #     if not self.token_id:
-    #         self.token_id = random.randint(100000, 999999)
-    #     if self.password and not self.password.startswith('pbkdf2_sha256$'):
-    #         self.password = make_password(self.password)
+    def save(self, *args, **kwargs):
+        if not self.token_id:
+            self.token_id = random.randint(100000, 999999)
+        if self.password and not self.password.startswith('pbkdf2_sha256$'):
+            self.password = make_password(self.password)
         
-    #     super().save(*args, **kwargs)
+        super().save(*args, **kwargs)

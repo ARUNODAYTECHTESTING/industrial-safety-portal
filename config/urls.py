fdb81417cce88f7ee89096fbfd089255ff92c097
Ubuntu <ubuntu@ip-172-31-9-93.ap-south-1.compute.internal>
2025-04-10 14:25:46 +0000

    server chnages commit

--- config/urls.py
+++ config/urls.py
@@ -26,7 +26,7 @@ if not settings.DEBUG:
             title="OCR API",
             default_version='v1',
         ),
-        url='http://3.110.183.233/',
+        url='http://3.7.253.20/',
         public=True,
         permission_classes=(permissions.AllowAny,),
     )

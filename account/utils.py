4c49bfde4689f79351082fbe35fbdbe625193444
Ubuntu <ubuntu@ip-172-31-9-93.ap-south-1.compute.internal>
2025-05-12 09:06:55 +0000

    initial commit

--- account/utils.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.hashers import make_password, check_password
-
-class PasswordManager:
-
-    @staticmethod
-    def hash_password(password):
-        return make_password(password = password)
